c43465976716e4d6e6793c1e68cf34a21759508a
Jeremy O'Brien <neutral@fastmail.com>
2019-12-08 13:32:21 -0500

    day 8 part 2 done

---
+++ 08/08-pt2.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+type LayerCounts map[byte]int
+
+func main() {
+	fmt.Println("*** PART 2 ***")
+	dat, err := ioutil.ReadFile(os.Args[1])
+	check(err)
+
+	constructedImage := make([]byte, 150)
+
+	for i := range constructedImage {
+		constructedImage[i] = '2'
+	}
+	for i, c := range dat[:len(dat)-1] {
+		if constructedImage[i % 150] == '2' && c != '2' {
+			constructedImage[i % 150] = c
+		}
+	}
+
+	for i, c := range constructedImage {
+		switch c {
+		case '0':
+			fmt.Printf(" ")
+		case '1':
+			fmt.Printf("X")
+		}
+		if (i+1) % 25 == 0 {
+			fmt.Println("")
+		}
+	}
+}
