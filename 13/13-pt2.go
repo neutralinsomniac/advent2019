439c9674aded9dfdcf37d0e78d5c59bf62cb3be7
Jeremy O'Brien <neutral@fastmail.com>
2019-12-13 22:42:56 -0500

    clean up day 13 and optimize for paddle/ball searching

--- 13/13-pt2.go
+++ 13/13-pt2.go
@@ -7,10 +7,6 @@ import (
 	"github.com/neutralinsomniac/advent2019/intcode"
 )
 
-// fuck you @jrick :)
-var score int
-var minX, minY, maxX, maxY int
-
 type TileID int
 
 const (
@@ -27,42 +23,33 @@ type Coord struct {
 
 type Board map[Coord]TileID
 
-func (b Board) GetPaddlePos() Coord {
-	for coord, tileID := range b {
-		if tileID == paddle {
-			return coord
-		}
-	}
-	return Coord{}
+type Game struct {
+	board                  Board
+	score                  int
+	paddle                 Coord
+	ball, prevball         Coord
+	minX, minY, maxX, maxY int
 }
 
-func (b Board) GetBallPos() Coord {
-	for coord, tileID := range b {
-		if tileID == ball {
-			return coord
+func (g *Game) Draw() {
+	fmt.Println("score:", g.score)
+	for coord := range g.board {
+		if coord.x > g.maxX {
+			g.maxX = coord.x
+		}
+		if coord.x < g.minX {
+			g.minX = coord.x
+		}
+		if coord.y > g.maxY {
+			g.maxY = coord.y
+		}
+		if coord.y < g.minY {
+			g.minY = coord.y
 		}
 	}
-	return Coord{}
-}
-
-func (b Board) Draw() {
-	for coord := range b {
-		if coord.x > maxX {
-			maxX = coord.x
-		}
-		if coord.x < minX {
-			minX = coord.x
-		}
-		if coord.y > maxY {
-			maxY = coord.y
-		}
-		if coord.y < minY {
-			minY = coord.y
-		}
-	}
-	for y := minY; y <= maxY; y++ {
-		for x := minX; x <= maxX; x++ {
-			for coord, tileID := range b {
+	for y := g.minY; y <= g.maxY; y++ {
+		for x := g.minX; x <= g.maxX; x++ {
+			for coord, tileID := range g.board {
 				if coord.x == x && coord.y == y {
 					switch tileID {
 					case wall:
@@ -94,7 +81,8 @@ func main() {
 	program.SetMemory(0, 2)
 	halted := false
 
-	board := make(Board)
+	game := Game{}
+	game.board = make(Board)
 	if draw {
 		fmt.Printf("\033[2J;\033[H")
 	}
@@ -112,10 +100,8 @@ func main() {
 		opcode, output, halted = program.RunUntilInputOrOutput()
 		switch opcode {
 		case intcode.Input:
-			// find the paddle
-			paddlepos := board.GetPaddlePos()
-			// find the ball
-			ballpos := board.GetBallPos()
+			paddlepos := game.paddle
+			ballpos := game.ball
 			// move the joystick
 			if paddlepos.x > ballpos.x {
 				program.SetReaderFromInts(-1)
@@ -126,25 +112,30 @@ func main() {
 			}
 			// and run the input instruction
 			program.Step()
-			if draw {
-				board.Draw()
-				fmt.Println("score:", score)
-			}
 		case intcode.Output:
 			switch output {
 			case -1:
 				// score
 				_, halted = program.RunUntilOutput()
-				score, halted = program.RunUntilOutput()
+				game.score, halted = program.RunUntilOutput()
 			default:
 				// x, y, tile:
 				x = output
 				y, halted = program.RunUntilOutput()
 				tmp, halted = program.RunUntilOutput()
 				tileID = TileID(tmp)
-				board[Coord{x, y}] = tileID
+				game.board[Coord{x, y}] = tileID
+				if tileID == ball {
+					game.ball = Coord{x, y}
+					if draw && game.ball != game.prevball {
+						game.Draw()
+						game.prevball = game.ball
+					}
+				} else if tileID == paddle {
+					game.paddle = Coord{x, y}
+				}
 			}
 		}
 	}
-	fmt.Println("score:", score)
+	fmt.Println("score:", game.score)
 }
